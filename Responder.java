110e0630018d574062bd4c5f1e1e736c96a66fe4
Spencer Gunning <swcgunning@gmail.com>
2021-12-11 02:09:40 -0500

    Fix up output issues

--- Responder.java
+++ Responder.java
@@ -14,8 +14,8 @@ import java.util.*;
  * in the HashMap, the corresponding response is returned. If none of the input
  * words is recognized, one of the default responses is randomly chosen.
  * 
- * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
+ * @author David J. Barnes and Michael Kölling and Spencer Gunning.
+ * @version 2021.12.10
  */
 public class Responder
 {
@@ -25,6 +25,8 @@ public class Responder
     private ArrayList<String> defaultResponses;
     // The name of the file containing the default responses.
     private static final String FILE_OF_DEFAULT_RESPONSES = "default.txt";
+    // The name of the file containing the respose hash map.
+    private static final String FILE_OF_RESPONSE_HASH_MAP = "default2.txt";
     private Random randomGenerator;
 
     /**
@@ -65,54 +67,52 @@ public class Responder
      * Enter all the known keywords and their associated responses
      * into our response map.
      */
-    private void fillResponseMap()
+     private void fillResponseMap()
     {
-        responseMap.put("crash", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("crashes", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("slow", 
-                        "I think this has to do with your hardware. Upgrading your processor\n" +
-                        "should solve all performance problems. Have you got a problem with\n" +
-                        "our software?");
-        responseMap.put("performance", 
-                        "Performance was quite adequate in all our tests. Are you running\n" +
-                        "any other processes in the background?");
-        responseMap.put("bug", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("buggy", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("windows", 
-                        "This is a known bug to do with the Windows operating system. Please\n" +
-                        "report it to Microsoft. There is nothing we can do about this.");
-        responseMap.put("macintosh", 
-                        "This is a known bug to do with the Mac operating system. Please\n" +
-                        "report it to Apple. There is nothing we can do about this.");
-        responseMap.put("expensive", 
-                        "The cost of our product is quite competitive. Have you looked around\n" +
-                        "and really compared our features?");
-        responseMap.put("installation", 
-                        "The installation is really quite straight forward. We have tons of\n" +
-                        "wizards that do all the work for you. Have you read the installation\n" +
-                        "instructions?");
-        responseMap.put("memory", 
-                        "If you read the system requirements carefully, you will see that the\n" +
-                        "specified memory requirements are 1.5 giga byte. You really should\n" +
-                        "upgrade your memory. Anything else you want to know?");
-        responseMap.put("linux", 
-                        "We take Linux support very seriously. But there are some problems.\n" +
-                        "Most have to do with incompatible glibc versions. Can you be a bit\n" +
-                        "more precise?");
-        responseMap.put("bluej", 
-                        "Ahhh, BlueJ, yes. We tried to buy out those guys long ago, but\n" +
-                        "they simply won't sell... Stubborn people they are. Nothing we can\n" +
-                        "do about it, I'm afraid.");
+        Charset charset = Charset.forName("US-ASCII");
+        Path path = Paths.get(FILE_OF_RESPONSE_HASH_MAP);
+        try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
+            String line = reader.readLine();
+            String keys = "";
+            String response = "";
+            int count = 0;
+            boolean append = false;
+            while(line != null) {
+                System.out.println(count);
+                if(line.trim().isEmpty()) {
+                    String[] keyArray = keys.split(", ");
+                    for (int i = 0; i < keyArray.length; i++) {
+                        responseMap.put(keyArray[i], response);
+                    }
+                    append = false;
+                    count = -1;
+                } else if (count == 0) {
+                    keys = line;  
+                } else if (count >= 1) {
+                    if(append) {
+                        response += "\n" + line;
+                    } else {
+                        response = line;
+                    }
+                    append = true;
+                }
+                count++;
+                line = reader.readLine();
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.err.println("Unable to open " + FILE_OF_DEFAULT_RESPONSES);
+        }
+        catch(IOException e) {
+            System.err.println("A problem was encountered reading " +
+                               FILE_OF_DEFAULT_RESPONSES);
+        }
+        // Make sure we have at least one response.
+        if(responseMap.size() == 0) {
+            responseMap.put("crash", 
+            "Well, it never crashes on our system. It must have something\n" +
+            "to do with your system. Tell me more about your configuration.");
+        }
     }
 
     /**
@@ -126,14 +126,21 @@ public class Responder
         try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
             String line = reader.readLine();
             String response = "";
-            int i = 0;
+            boolean append = false;
             while(line != null) {
-                while(line.trim().isEmpty() != true) {
-                    response = line;
-                    System.out.println(i + " " + response);
-                    line = reader.readLine();
+                if(line.trim().isEmpty() != true) {
+                    if(append) {
+                        response += "\n" + line;
+                    } else {
+                        response = line;
+                    }
+                    append = true;
+                } else {
+                    if (append) {
+                        defaultResponses.add(response);
+                    }
+                    append = false;
                 }
-                i++;
                 line = reader.readLine();
             }
         }
